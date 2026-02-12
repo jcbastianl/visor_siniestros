6d900860cb17c26d204146818bcf3e085ee84ec8
joseph <joseph.balcazar@unl.edu.ec>
2026-02-12 10:55:26 -0500

    Fix: Rutas de despliegue en render.yaml y build.sh

--- build.sh
+++ build.sh
@@ -2,8 +2,10 @@
 # exit on error
 set -o errexit
 
+cd visor_siniestros
+
 # Install dependencies
-pip install -r visor_siniestros/requirements.txt
+pip install -r requirements.txt
 
 # Collect static files
 python manage.py collectstatic --no-input
